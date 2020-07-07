9e2f4dca123273b1cc51fd8221a8bbeac41f034a
devhoons <devjhyang@gmail.com>
2020-07-07 17:47:28 +0900

    photo 앱 관리자 페이지 커스터마이징

--- photo/admin.py
+++ photo/admin.py
@@ -4,4 +4,8 @@ from .models import Photo
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "created_at", "updated_at"]
+    raw_id_fields = ["name"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["name__username", "text", "created_at"]
+    ordering = ["-updated_at", "-created_at"]
