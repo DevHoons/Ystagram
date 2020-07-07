c2b06b6099d86376682ff9897ace9bbae4c9d10d
devhoons <devjhyang@gmail.com>
2020-07-07 22:08:22 +0900

    유저모델과 Django Form에서 비밀번호 확인하는 기능을 추가하여 회원가입 만들기

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
-
+from . import views
 
 urlpatterns = [
     path("login/", auth_view.LoginView.as_view(), name="login"),
@@ -9,4 +9,5 @@ urlpatterns = [
         auth_view.LogoutView.as_view(template_name="registration/logout.html"),
         name="logout",
     ),
+    path("signup/", views.signup, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        user_form = SignUpForm(request.POST)
+        if user_form.is_valid():
+            create_user = user_form.save(commit=False)
+            create_user.set_password(user_form.cleaned_data["password"])
+            create_user.save()
+            return render(
+                request, "registration/signup_done.html", {"create_user": create_user}
+            )
+    else:
+        user_form = SignUpForm()
+
+    return render(request, "registration/signup.html", {"form": user_form})
