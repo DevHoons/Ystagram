d8183a26a37321449cbdea305740a6b428657c4c
devhoons <devjhyang@gmail.com>
2020-07-07 20:48:40 +0900

    CBV로 Create, Delete, Update, Detail View 만들기

--- photo/urls.py
+++ photo/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
+from django.views.generic.detail import DetailView
 from . import views
+from .models import Photo
 
 app_name = "photo"
 
 urlpatterns = [
     path("", views.photo_list, name="photo_list"),
+    path(
+        "detail/<int:pk>/",
+        DetailView.as_view(model=Photo, template_name="detail.html"),
+        name="photo_detail",
+    ),
+    path("upload/", views.PhotoUploadView.as_view(), name="photo_upload"),
+    path("delete/<int:pk>/", views.PhotoDeleteView.as_view(), name="photo_delete"),
+    path("update/<int:pk>/", views.PhotoUpdateView.as_view(), name="photo_update"),
 ]
--- photo/views.py
+++ photo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Photo
 
 
@@ -6,3 +7,29 @@ def photo_list(request):
     photos = Photo.objects.all()
     ctx = {"photos": photos}
     return render(request, "list.html", ctx)
+
+
+class PhotoUploadView(CreateView):
+    model = Photo
+    fields = ["photo", "text"]
+    template_name = "upload.html"
+
+    def form_valid(self, form):
+        form.instance.name_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect("/")
+        else:
+            return self.render_to_response({"form": form})
+
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    success_url = "/"
+    template_name = "delete.html"
+
+
+class PhotoUpdateView(UpdateView):
+    model = Photo
+    fields = ["photo", "text"]
+    template_name = "update.html"
