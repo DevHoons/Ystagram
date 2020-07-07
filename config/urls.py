1a59f923c64c7ffc925872cf64e646e900c612a5
devhoons <devjhyang@gmail.com>
2020-07-08 00:55:28 +0900

    DEBUG = False 설정 후 미디어 파일 제공해주는 설정

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.static import serve
+from django.urls import re_path
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -22,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("photo.urls", namespace="photo_list")),
     path("accounts/", include("accounts.urls")),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
